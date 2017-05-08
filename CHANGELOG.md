566cbcf12fcb1631c2038510151c88f71054e3a9
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2017-05-08 14:51:31 -0500

    downloading basic profile data

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 2017.05.08
+
+- Registered for the [Consumer App Partner
+  Program](https://developer.linkedin.com/partner-programs/consumer) so we can
+  download more data
--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,18 @@
-function hello_world() {
-    console.log('hi!')
+function main() {
+
     console.log(IN.User.isAuthorized())
+    IN.User.authorize(onAuthentication)
+}
+
+function onAuthentication() {
+    console.log(IN.User.isAuthorized())
+
+    // lots of things available here. more details at:
+    // https://developer.linkedin.com/docs/fields/basic-profile
+    // http://stackoverflow.com/q/8593144/564709
+    IN.API.Raw("/people/~:(id,picture-url,industry,positions,skills,educations:(id,school-name,degree,field-of-study))?format=json").result(storePositionHistory)
+}
+
+function storePositionHistory(data) {
+    console.log(data)
 }
